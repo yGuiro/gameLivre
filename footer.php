bb95b321a8bda45bf50254aac2fc8f5506304e61
yGuiro <108142728+yGuiro@users.noreply.github.com>
2022-08-24 13:47:48 -0300

    24-08-22

---
+++ footer.php
@@ -0,0 +1,21 @@
+<?php include 'head.php' ?>
+
+<footer>
+    <section>
+        <div class="rodape">
+            <ul>
+                <li>
+                    <div class="txt">
+                        <h2>SOBRE A GAME LIVRE</h2><br>
+                        <p>&nbsp;&nbsp;&nbsp;A Game Livre é um Trabalho de Conclusão de Curso (TCC)<br> 
+                        produzido por Guilherme, Rodrigo, Carlos e Alexia.<br>
+                        Nosso site trabalha com locação e venda de jogos entre usuário e usuário,<br>
+                        podendo serem pagos com dinheiro real ou <br>
+                        com nosso sistema de pontos obtidos ao fazer missões dadas no site.</p>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </section>
+</footer>
+

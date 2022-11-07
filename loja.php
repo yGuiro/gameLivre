e307a2e90c9aff7e95193ee3f8341bccca601515
Guilherme Ferreira <108142728+yGuiro@users.noreply.github.com>
2022-11-07 16:33:41 -0300

    Delete loja.php

--- loja.php
+++
@@ -1,8 +0,0 @@
-<link rel="shortcut icon" href="/TCC/img/fav.png" type="image/x-icon">
-<body>
-    <?php include 'head.php'; ?>
-    <?php include 'navbar.php'; ?>
-    <?php include 'footer.php'; ?>
-    
-</body>
-</html>
